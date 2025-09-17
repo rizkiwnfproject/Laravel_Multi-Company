4e95c93c222f40e59a08a70a0c4d97f252fa3da5
rizkiwnfproject <rizkiwahyuproject@gmail.com>
2025-09-17 21:20:02 +0700

    homepage integration

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CompanyStatistic;
+use App\Models\HeroSection;
+use App\Models\OurPrinciple;
+use App\Models\OurTeam;
+use App\Models\Product;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    public function index(){
-        return view('front.index');
+    public function index()
+    {
+        $statistics = CompanyStatistic::take(4)->get();
+        $principles = OurPrinciple::take(3)->get();
+        $products = Product::take(3)->get();
+        $teams = OurTeam::take(7)->get();
+        $testimonials = Testimonial::take(5)->get();
+        $hero_sections = HeroSection::orderByDesc('id')->take(1)->get();
+
+        return view('front.index', compact('statistics', 'principles', 'products', 'teams', 'testimonials', 'hero_sections'));
     }
 }
